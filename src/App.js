8bb99a7bee8fd48b4ba98c40b4a71d8341e34bb5
christopher lopez <christopherwlopez89@gmail.com>
2024-02-26 18:37:53 -0800

    synth started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+let actx = new AudioContext();
+let out = actx.destination;
+let osc1 = actx.createOscillator();
+let gain1 = actx.createGain();
+let filter = actx.createBiquadFilter();
+
+osc1.connect(gain1);
+gain1.connect(filter);
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
