07682c57d98bb0db3bc4847cbf2ba1eb5d8f9d55
christopher lopez <christopherwlopez89@gmail.com>
2024-02-26 18:53:09 -0800

    state set

--- src/App.js
+++ src/App.js
@@ -9,12 +9,25 @@ let filter = actx.createBiquadFilter();
 
 osc1.connect(gain1);
 gain1.connect(filter);
-
+filter = actx.createBiquadFilter();
 
 const App = () => {
+
+  const [osc1Settings, setosc1Settings] = useState({
+    frequency: osc1.frequency.value,
+    detune: osc1.detune.value,
+    type: osc1.type
+  });
+
+  const changeOsc1 = e =>{
+    let {value, id} = e.target;
+    setosc1Settings({...osc1Settings, [id]: value});
+  }
+  
   return (
     <div>
       
+
     </div>
   )
 }
